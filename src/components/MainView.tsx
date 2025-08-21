d3b81e931467309e8da4854bd97236e596b8344b
Kenneth Vu <kvu@it.ucla.edu>
2025-08-21 16:45:35 -0700

    Update MainView.tsx

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,6 +1,6 @@
-import { ControlStateProvider, useControlContext } from "../hooks/ControlStateContext";
-import SplashScreen from "./SplashScreen.tsx";
-import MainScreen from "./MainScreen.tsx";
+// import {ControlStateProvider, useControlContext} from "../hooks/ControlStateContext.tsx";
+// import SplashScreen from "./SplashScreen.tsx";
+// import MainScreen from "./MainScreen.tsx";
 import {Navigate, useParams} from "react-router-dom";
 
 export default function MainView() {
@@ -8,15 +8,17 @@ export default function MainView() {
     
     if (!system_id) return <Navigate to="/404" replace />;
 
+
     return (
-        <ControlStateProvider systemID={system_id}>
-            <MainViewInner />
-        </ControlStateProvider>
+        <div>Hello World from {system_id}</div>
+        // <ControlStateProvider systemID={system_id}>
+        //     <MainViewInner />
+        // </ControlStateProvider>
     );
 }
 
-function MainViewInner() {
-    const { active } = useControlContext();
-    return active ? <MainScreen /> : <SplashScreen />;
-}
+// function MainViewInner() {
+//     const { active } = useControlContext();
+//     return active ? <MainScreen /> : <SplashScreen />;
+// }
 
