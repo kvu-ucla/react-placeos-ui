55613744ad4e1e1d97054eda8c43f9e47e260c3c
Kenneth Vu <kvu@it.ucla.edu>
2025-08-21 15:56:37 -0700

    updated main view

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,13 +1,23 @@
-import { useControlContext} from "../hooks/ControlStateContext.tsx";
+import {ControlStateProvider, useControlContext} from "../hooks/ControlStateContext.tsx";
 import SplashScreen from "./SplashScreen.tsx";
 import MainScreen from "./MainScreen.tsx";
+import {Navigate, useParams} from "react-router-dom";
 
 export default function MainView() {
-    const { active } = useControlContext();
+    const { system_id } = useParams();
+
+    // If your route guarantees :system_id, you can assert; otherwise guard:
+    if (!system_id) return <Navigate to="/404" replace />; // or return null / a fallback
 
     return (
- active ? <MainScreen /> : <SplashScreen />
+        <ControlStateProvider systemID={system_id}>
+            <MainViewInner />
+        </ControlStateProvider>
     );
 }
 
+function MainViewInner() {
+    const { active } = useControlContext();
+    return active ? <MainScreen /> : <SplashScreen />;
+}
 
