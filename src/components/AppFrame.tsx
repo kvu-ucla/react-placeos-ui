7744b5eba974299052a16306ebd1658630b4550c
Kenneth Vu <kvu@it.ucla.edu>
2025-09-11 07:21:09 -0700

    updated appframe with dpr

--- src/components/AppFrame.tsx
+++ src/components/AppFrame.tsx
@@ -1,48 +1,53 @@
-// AppFrame.tsx
 import * as React from "react";
 
 export default function AppFrame({ children }: { children: React.ReactNode }) {
-  const DESIGN_W = 1920,
-    DESIGN_H = 1200;
-  const [vw, setVw] = React.useState(window.innerWidth);
-  const [vh, setVh] = React.useState(window.innerHeight);
+    const DESIGN_W = 1920;
+    const DESIGN_H = 1200;
 
-  React.useEffect(() => {
-    const onResize = () => {
-      setVw(window.innerWidth);
-      setVh(window.innerHeight);
-    };
-    window.addEventListener("resize", onResize);
-    window.visualViewport?.addEventListener("resize", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-      window.visualViewport?.removeEventListener("resize", onResize);
-    };
-  }, []);
+    const [vw, setVw] = React.useState(window.innerWidth);
+    const [vh, setVh] = React.useState(window.innerHeight);
 
-  const scale = Math.min(vw / DESIGN_W, vh / DESIGN_H);
-  const scaledW = DESIGN_W * scale;
-  const scaledH = DESIGN_H * scale;
-  const left = Math.round((vw - scaledW) / 2);
-  const top = Math.round((vh - scaledH) / 2);
+    React.useEffect(() => {
+        const onResize = () => {
+            setVw(window.innerWidth);
+            setVh(window.innerHeight);
+        };
+        window.addEventListener("resize", onResize);
+        window.visualViewport?.addEventListener("resize", onResize);
+        return () => {
+            window.removeEventListener("resize", onResize);
+            window.visualViewport?.removeEventListener("resize", onResize);
+        };
+    }, []);
 
-  return (
-    <div
-      id="app-frame"
-      style={{
-        // key: positioned container so Reactour can use absolute/inset:0 inside
-        position: "fixed",
-        left,
-        top,
-        width: DESIGN_W,
-        height: DESIGN_H,
-        transform: `scale(${scale})`,
-        transformOrigin: "top left",
-        background: "black",
-        overflow: "hidden",
-      }}
-    >
-      {children}
-    </div>
-  );
+    const dpr = window.devicePixelRatio || 1;
+
+    const effectiveVw = vw * dpr;
+    const effectiveVh = vh * dpr;
+
+    const scale = Math.min(effectiveVw / DESIGN_W, effectiveVh / DESIGN_H);
+    const scaledW = DESIGN_W * scale;
+    const scaledH = DESIGN_H * scale;
+
+    const left = Math.round((effectiveVw - scaledW) / 2) / dpr;
+    const top = Math.round((effectiveVh - scaledH) / 2) / dpr;
+
+    return (
+        <div
+            id="app-frame"
+            style={{
+                position: "fixed",
+                left,
+                top,
+                width: DESIGN_W,
+                height: DESIGN_H,
+                transform: `scale(${scale / dpr})`,
+                transformOrigin: "top left",
+                background: "black",
+                overflow: "hidden",
+            }}
+        >
+            {children}
+        </div>
+    );
 }
