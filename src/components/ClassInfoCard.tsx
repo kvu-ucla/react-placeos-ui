d7deb6ad5b44dd5651e8095ece9172af3e83ba50
Kenneth Vu <kvu@it.ucla.edu>
2025-09-21 18:07:51 -0700

    Update ClassInfoCard.tsx

--- src/components/ClassInfoCard.tsx
+++ src/components/ClassInfoCard.tsx
@@ -132,36 +132,39 @@ export function ClassInfoCard() {
             <div>{upcoming}</div>
           </div>
 
-          <div className="font-semibold">
-                  Sharing Key: {sharing?.directPresentationSharingKey}
+          <div className="flex items-center justify-center gap-2">
+            {/*Sharing Key*/}
+            <div className="font-semibold">
+              Sharing Key: {sharing?.directPresentationSharingKey}
             </div>
-
-          {/*BruinCasting State*/}
-          {recording && (
-              <div className="flex flex-col items-center p-2">
-                <div className="inline-flex justify-evenly items-center bg-gray-400/15 rounded-[10px] px-4 py-4">
-                  <div className="relative">
-                    <div className="absolute inline-flex h-4 w-4 rounded-full bg-[#48E960] opacity-75 animate-ping"></div>
-                    <div className="relative h-4 w-4 bg-[#48E960] rounded-full mr-4"></div>
+            {/*BruinCasting State*/}
+            {recording && (
+                <div className="flex flex-col items-center p-2">
+                  <div className="inline-flex justify-evenly items-center bg-gray-400/15 rounded-[10px] px-4 py-4">
+                    <div className="relative">
+                      <div className="absolute inline-flex h-4 w-4 rounded-full bg-[#48E960] opacity-75 animate-ping"></div>
+                      <div className="relative h-4 w-4 bg-[#48E960] rounded-full mr-4"></div>
+                    </div>
+                    <div className="font-semibold">BruinCasting</div>
                   </div>
-                  <div className="font-semibold">BruinCasting</div>
+                  <div className="text-lg text-gray-300">
+                    Recording powered by BruinCast
+                  </div>
                 </div>
-                <div className="text-lg text-gray-300">
-                  Recording powered by BruinCast
+            )}
+            {!recording && (
+                <div className="flex flex-col items-center p-2">
+                  <div className="inline-flex justify-evenly items-center bg-[#001A5C] rounded-[10px] px-4 py-4">
+                    <div className="h-4 w-4 bg-[#CCCCCC] rounded-full mr-4"></div>
+                    <div className="font-semibold">Not Bruincasting</div>
+                  </div>
+                  <div className="text-lg text-gray-300">
+                    Recording powered by BruinCast
+                  </div>
                 </div>
-              </div>
-          )}
-          {!recording && (
-              <div className="flex flex-col items-center p-2">
-                <div className="inline-flex justify-evenly items-center bg-[#001A5C] rounded-[10px] px-4 py-4">
-                  <div className="h-4 w-4 bg-[#CCCCCC] rounded-full mr-4"></div>
-                  <div className="font-semibold">Not Bruincasting</div>
-                </div>
-                <div className="text-lg text-gray-300">
-                  Recording powered by BruinCast
-                </div>
-              </div>
-          )}
+            )}
+          </div>
+
         </>
       ) : (
         <>
