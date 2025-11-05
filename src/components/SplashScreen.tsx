7f004b47bd59644db4be7bb291935281340261c3
Kenneth Vu <kvu@it.ucla.edu>
2025-11-04 19:52:46 -0800

    updated survey

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,5 @@
 import { Header } from "./Header";
-import { ClassInfoCard } from "./ClassInfoCard";
+// import { ClassInfoCard } from "./ClassInfoCard";
 import { useControlContext } from "../hooks/ControlStateContext";
 import { ModalProvider } from "../hooks/ModalContext";
 import { useZoomContext } from "../hooks/ZoomContext";
